713e78d3c1d239ddc48dea8cdf93decf031e1560
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2025-12-16 13:24:27 +0530

    feat: add new input components for OTP, multi-select, text area, phone, radio, rating, number, select, date, and address mini-form.

--- components/AppInputFields/components/InputDate.tsx
+++ components/AppInputFields/components/InputDate.tsx
@@ -110,7 +110,7 @@ const InputDate = <T extends FieldValues>({
       render={({ field }) => (
         <FormItem
           className={cn(
-            "w-full max-w-[400px]",
+            "w-full",
             "group transition-all duration-300 ease-in-out",
             className
           )}
