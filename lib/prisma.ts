847a6d0ec33da17bc77e4f54c25cc17bc0c312db
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2025-12-16 10:57:52 +0530

    feat: Add Prisma client initialization with singleton pattern for database access.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "@/lib/generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
