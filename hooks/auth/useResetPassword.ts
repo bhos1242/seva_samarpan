f40214c8887122540af442b5d49448928b9f1036
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2025-12-18 12:25:44 +0530

    feat: add useResetPassword hook for handling password reset functionality

---
+++ hooks/auth/useResetPassword.ts
@@ -0,0 +1,25 @@
+import { useMutation } from "@tantml:function_calls>
+<invoke name="create_file">
+<parameter name="content">import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+interface ResetPasswordData {
+  token: string;
+  password: string;
+}
+
+interface ResetPasswordResponse {
+  message: string;
+}
+
+export function useResetPassword() {
+  return useMutation({
+    mutationFn: async (data: ResetPasswordData) => {
+      const response = await axios.post<ResetPasswordResponse>(
+        "/api/auth/reset-password",
+        data
+      );
+      return response.data;
+    },
+  });
+}
